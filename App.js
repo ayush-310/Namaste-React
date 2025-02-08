df7c5e28de94dea585fe827feb3fb465d62f8430
Ayush <ayuofficial11310@gmail.com>
2025-02-08 12:50:47 +0530

    React Element

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
-
-// Nested Elements in React
-{/* <div class="parent">
-    <div class="child">
-        <h1>I am H1</h1>
-        <h2>I am H2</h2>
-    </div>
-</div> */}
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { className: "parent" },
-    React.createElement("div", { className: "child" }, [
-        React.createElement("h1", {}, "I am H1"),
-        React.createElement("h2", {}, "I am H2")
-    ]),
-    React.createElement("div", { className: "child2" }, [
-        React.createElement("h1", {}, "I am H1"),
-        React.createElement("h2", {}, "I am H2")
-    ]));
+// React Element
+// JSX - HTML-Like or XML-like syntax
+// JSX (transpiled before it reaches to the JSX) - PARCEL - Babel
+const element = (
+    <h1 className="head">
+        Hello World using JSX 😛
+    </h1>
+);
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
-
+// Create root and render element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
